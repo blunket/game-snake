3c490fc74e43f002304700975970b20abf32d961
blunket <blerrnk@gmail.com>
2016-03-03 00:14:51 -0600

    Update game.js
    snake can grow, game speed can vary, fixed collision at right and bottom edges, can reset after gameover

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ var turnedThisFrame = false; //used to disallow turning twice in one frame
 
 function gameLoop(firstrun) {
     
+    gameSpeed = document.getElementById("speed").value;
+    
     turnedThisFrame = false;
     
     ctx.clearCanvas(); //defined in setup.js
@@ -41,15 +43,23 @@ function gameLoop(firstrun) {
         ctx.fillRect(Snake.nodes[i][0] * squareSize, Snake.nodes[i][1] * squareSize, squareSize, squareSize);
         if (Snake.nodes[i][0] == Snake.x && Snake.nodes[i][1] == Snake.y) {
             Snake.moving = false;
+            Snake.init();
             ctx.fillText("Game over (space to restart)", canvas.width / 2, canvas.height / 2);
             break;
         }
     }
     
+    //draw pellet, but smaller
+    ctx.fillRect(Food.x * squareSize + (.25 * squareSize), Food.y * squareSize + (.25 * squareSize), squareSize / 2, squareSize / 2);
+    
     //collision detection
-    if (Snake.x > gridWidth || Snake.x < 0 || Snake.y > gridHeight || Snake.y < 0) {
+    if (Snake.x >= gridWidth || Snake.x < 0 || Snake.y >= gridHeight || Snake.y < 0) {
         Snake.moving = false;
+        Snake.init();
         ctx.fillText("Game over (space to restart)", canvas.width / 2, canvas.height / 2);
+    } else if (Snake.x == Food.x && Snake.y == Food.y) {
+        Snake.curlen++;
+        Food.init();
     }
     
     //print instructions if on setup
@@ -58,11 +68,10 @@ function gameLoop(firstrun) {
         ctx.fillText("Space to start", canvas.width / 2, canvas.height / 2);
     }
     
-    if (Snake.moving) setTimeout(gameLoop, 50);
+    if (Snake.moving) setTimeout(gameLoop, 100 - gameSpeed * 5);
 }
 
 canvas.addEventListener("keydown", function(e){
-    
     if (Snake.moving) {
         //does not let the player turn directly around
         if ((e.keyCode == 37 && Snake.dir !== 39) || (e.keyCode == 39 && Snake.dir !== 37) || (e.keyCode == 38 && Snake.dir !== 40) || (e.keyCode == 40 && Snake.dir !== 38))
